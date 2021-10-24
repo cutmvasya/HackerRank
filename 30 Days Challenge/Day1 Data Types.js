b30a0aeb00b17eed3b46730a1a3a30491b00d7f7
cutmvasya <meutia.vasya@gmail.com>
2021-10-24 22:45:51 +0700

    Data Types

---
+++ 30 Days Challenge/Day1 Data Types.js
@@ -0,0 +1,13 @@
+// Declare second integer, double, and String variables.
+let int, double, string;
+// Read and save an integer, double, and String to your variables.
+int = parseInt(readLine());
+double = parseFloat(readLine());
+string = readLine()
+    // Print the sum of both integer variables on a new line.
+console.log(i + int)
+    // Print the sum of the double variables on a new line.
+console.log((d + double).toFixed(1))
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+console.log(s + string)
